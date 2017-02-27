6862a5cbc3d32bd8ff393c5e88d022b342e64a28
Charlotte Hearne <charlotte.hearne@ucdconnect.ie>
2017-02-27 22:32:38 +0000

    Refactored tests for new class in main.py

--- Assignment3/tests/tests.py
+++ Assignment3/tests/tests.py
@@ -1,31 +1,47 @@
-# from nose.tools import *
-# from nose.tools.trivial import eq_, ok_
-from SE.Assignment3.light_box import parse_line, switch, on, off
+from light_box import parse_line, switch, on, off, LightBox, read_uri
 import unittest
 
-class TestSetAdt(unittest.TestCase):
+
+
+class TestLights(unittest.TestCase):
+
+    def setUp(self):
+        self.light_box = LightBox(3)
+        self.test_buffer = ['3', 'switch 0,1 through 2,2']
+        self.test_file = "light_box/input_assign3_b_v2.txt"
+
+    def test_read_file(self):
+        read_uri(self.test_file)
+
+
+    def test_light_change(self):
+        new_light = [[True, True, True],[False, False, False],[False, False, False]]
+        self.light_box.light_change('0, 0', '0, 2', on)
+        self.assertEquals(self.light_box.light, new_light)
+
+    def test_get_coords(self):
+        self.assertEquals(self.light_box.get_coords('-4, 7'), (0, 2))
+        self.assertEquals(self.light_box.get_coords('1, 2'), (1, 2))
+
+
     def test_parse_line_switch(self):
         start, end, fun = parse_line('switch 322,558 through 977,958')
-        assert start == (322, 558)
-        assert end == (977, 958)
-
+        self.assertEquals(start, '322,558' )
+        self.assertEquals(end, '977,958')
 
     def test_parse_line_onoff(self):
-        start, end, fun = parse_line('turn off 87,577 through 484,608')
-        assert start == (87,577)
-        assert end == (484,608)
+        start, end, fun = parse_line('turn off 87,577 through 484,-608')
+        self.assertEquals(start, '87,577')
+        self.assertEquals(end, '484,-608')
 
-    def test_on(self):
-        assert on(True) is True
-        assert on(False) is True
+    def test_on_off_switch(self):
+        self.assertTrue(on(True))
+        self.assertTrue(on(False))
+        self.assertFalse(off(True))
+        self.assertFalse(off(False))
+        self.assertFalse(switch(True))
+        self.assertTrue(switch(False))
 
-    def test_off(self):
-        assert off(True) is False
-        assert off(False) is False
-
-    def test_switch(self):
-        assert switch(True) is False
-        assert switch(False) is True
 
 if __name__ == '__main__':
     unittest.main()
